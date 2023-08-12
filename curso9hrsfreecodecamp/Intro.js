b5d019071549a1f9e1739d2e770b15ee0f9a7af6
Chudopie <lalolu94@hotmail.com>
2023-08-12 01:47:25 -0600

    1hra 14min 36s. freecodecamp

---
+++ curso9hrsfreecodecamp/Intro.js
@@ -0,0 +1,32 @@
+/*https://www.youtube.com/watch?v=ivdTnPl1ND0&t=399s 
+tipos de datos 
+Undefined, null, boolean, string, symbol, object.*/
+//este si quiero recordarlo
+var a =23;
+a +=5; //sumale 5 a la variable "a" y asignalo a la variable
+console.log(a);
+//NOICE
+let miCadena = "Soy una cadena de caracteres  \"con comillas\"   " //alt+92 \\ para las barras invertidas
+
+//ESO si no sabia como poner comillas adentro de los caracteres deseaooos
+console.log(miCadena);
+
+//o tambien
+let miCadena2 = 'Soy otra cadena pero" con comillas simples"'
+console.log(miCadena2);
+
+//Secuencias de escape
+/*Codigo ||         Resultado 
+\'                  comilla simple 
+\"                  comilla doble
+\\                  barra invertida se deben de usar dos barras invertidas para que se muestren
+\n                  linea nueva
+\r                  retorno de carro
+\t                  tabulacion
+\b                  retroceso
+\f                  salto de pagina
+.... //investigar mas en doc. de javascript
+
+console.log ( "Estoy aprendiendo \\\\ JAvascript ") //barra invertida
+console.log ( "Estoy aprendiendo: \n JAvascript ") // linea nueva \n
+*/
